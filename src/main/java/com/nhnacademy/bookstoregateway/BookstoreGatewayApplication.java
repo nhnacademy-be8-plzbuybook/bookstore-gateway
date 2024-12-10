7e09806373ff5414a9408856d6cfacebf236db6c
jalyice <jalyice@gmail.com>
2024-12-10 15:11:51 +0900

    first commit

---
+++ src/main/java/com/nhnacademy/bookstoregateway/BookstoreGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.bookstoregateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstoreGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookstoreGatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nhnacademy/bookstoregateway/BookstoreGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.bookstoregateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoreGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
