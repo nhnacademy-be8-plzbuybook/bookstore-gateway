1f6ddbaef70b2ea65ce238f3b77def409050f608
yunjiho <yoonwlgh12@gmail.com>
2024-12-11 10:27:59 +0900

    모든 요청을 gateway를 향해 지나가게 하는 설정 구현

--- src/main/java/com/nhnacademy/bookstoregateway/config/RouteLocatorConfig.java
+++ src/main/java/com/nhnacademy/bookstoregateway/config/RouteLocatorConfig.java
@@ -10,5 +10,26 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @RequiredArgsConstructor
 public class RouteLocatorConfig {
+    //라우터 설정, 모든 요청이 gateway를 지나게 한다.
     private final JwtAuthorizationHeaderFilter jwtAuthorizationHeaderFilter;
+
+
+    @Bean
+    public RouteLocator myRoute(RouteLocatorBuilder builder) {
+        return builder.routes()
+                //인증
+                //TODO account api 경로 p.path에 설정 (아직 account 경로 정하지 않음)
+                .route("bookstore-authentication-api", p -> p.path("/api/account/**")
+                        .filters(f->f.filter(jwtAuthorizationHeaderFilter.apply(new JwtAuthorizationHeaderFilter.Config())))
+                        .uri("lb://localhost:8082")
+                )
+                //쇼핑몰
+                //TODO account api 경로 p.path에 설정 (아직 shoppingmall 경로 정하지 않음)
+                .route("bookstore-shoppingmall", p -> p.path("/api/shop/***")
+                                .and()
+//                              .weight("bookstore-shoppingmall", 50)  쿠폰 서버가 분리 될 경우 추가
+                                .uri("lb://localhost:8081")
+                ).build();
+
+    }
 }
