51cf4c0389695678c4bc87f7252372b928bfbab4
jalyice <jalyice@gmail.com>
2024-12-23 16:12:15 +0900

    test 용 object storage route 추가

--- src/main/java/com/nhnacademy/bookstoregateway/config/RouteLocatorConfig.java
+++ src/main/java/com/nhnacademy/bookstoregateway/config/RouteLocatorConfig.java
@@ -19,11 +19,15 @@ public class RouteLocatorConfig {
     public RouteLocator myRoute(RouteLocatorBuilder builder) {
         return builder.routes()
                 .route("auth", p -> p.path("/api/auth/**")
-                        .uri("lb://bookstore-authentication-api")
+                        .uri("lb://AUTHENTICATION-DEV")
                 )
                 .route("shoppingmall", p -> p.path("/api/members/**")
                         .filters(f -> f.filter(jwtAuthorizationHeaderFilter.apply(new JwtAuthorizationHeaderFilter.Config())))
-                        .uri("lb://bookstore-shoppingmall")
+                        .uri("lb://BOOKSTORE-BACK2")
+                )
+                // 권한 부여 필요.
+                .route("bookstore", p->p.path("/api/objects/**")
+                        .uri("lb://BOOKSTORE-BACK2")
                 )
                 .build();
     }
